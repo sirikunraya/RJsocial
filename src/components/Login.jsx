a20eea7b98a1c71b1d97dc6ee7a60afb55c01dfd
sirikunraya <fiwring2000@gmail.com>
2019-12-26 15:21:30 +0700

    แก้ไขข้อความหน้า Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>เข้าสู่ระบบ</h1>
+                <h1>เข้าสู่ระบบของสิริกัลยานะจ๊ะ</h1>
             </div>
         );
     }
